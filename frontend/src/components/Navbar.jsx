aecf31da28e9dd5a828e7c2366e660c81358804e
leventsamlioglu <leventsamlioglu@icloud.com>
2023-07-03 13:44:19 +0200

    add Navbar comp

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+const Navbar = ({ username }) => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark justify-content-between mb-4 p-4 w-100">
+			<Link to="/" className="navbar-brand" style={{ fontSize: "2em" }}>
+				Matrix Master Community
+			</Link>
+
+			<ul className="navbar-nav mr-auto" style={{ fontSize: "1.5em" }}>
+				{username ? (
+					<>
+						<li className="nav-item active">
+							<p className="nav-link">
+								Welcome: <i>{username}</i>
+							</p>
+						</li>
+						<li className="nav-item active">
+							<Link to='/profile/<%=userId%>' className="nav-link">
+								Profile
+							</Link>
+						</li>
+						<li className="nav-item active">
+							<Link to="/logout" className="nav-link">
+								Logout
+							</Link>
+						</li>
+					</>
+				) : (
+					<>
+						<li className="nav-item active">
+							<Link to="/login" className="nav-link">
+								Login
+							</Link>
+						</li>
+						<li className="nav-item active">
+							<Link to="/signup" className="nav-link">
+								{" "}
+								Sign up{" "}
+							</Link>
+						</li>
+					</>
+				)}
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
