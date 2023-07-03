8c8b4c0a573de229ae246f2b88963d75c7873ec4
leventsamlioglu <leventsamlioglu@icloud.com>
2023-07-03 13:43:31 +0200

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<SignUp />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
