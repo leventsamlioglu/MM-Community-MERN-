30b161668e9d164b1ab35a56c66be950fc3552b2
leventsamlioglu <leventsamlioglu@icloud.com>
2023-07-05 15:42:52 +0200

    fix bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Details from "./pages/Details";
@@ -17,11 +16,9 @@ function App() {
 					<Route path="/" element={<HomePage />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/signup" element={<SignUp />} />
-					<Route path="/logout" element={<HomePage />} />
 					<Route path="/posts/create/:id" element={<Details />} />
 					<Route path="/editPost/:id" element={<EditModal />} />
 				</Routes>
-				<Footer />
 			</BrowserRouter>
 		</div>
 	);
